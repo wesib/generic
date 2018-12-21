bf065a6ca34505f357d0b00e7641a5695c67068b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-12-21 13:16:14 +0700

    Allow to bind to another `ValueTracker`

--- src/common/value-tracker.ts
+++ src/common/value-tracker.ts
@@ -1,9 +1,14 @@
-import { EventProducer } from 'fun-events';
+import { EventEmitter, EventInterest, EventProducer } from 'fun-events';
 
 /**
  * Value accessor and changes tracker.
  */
 export abstract class ValueTracker<T = any, N extends T = T> {
+
+  /**
+   * @internal
+   */
+  private _by = EventInterest.none;
 
   /**
    * Value changes event producer.
@@ -26,4 +31,81 @@ export abstract class ValueTracker<T = any, N extends T = T> {
    */
   abstract set it(value: T);
 
+  /**
+   * Bind the tracked value to the `source`.
+   *
+   * Updates the value when the `source` changes.
+   *
+   * If the value is already bound to another source, then unbinds from the old source first.
+   *
+   * @param source The value source.
+   */
+  by(source: ValueTracker<T>) {
+    this.off();
+    this._by = source.on(value => this.it = value);
+    this.it = source.it;
+  }
+
+  /**
+   * Unbinds the tracked value from the source.
+   *
+   * After this call the tracked value won't be updated on the source modification.
+   *
+   * If the value is not bound then doe nothing.
+   */
+  off() {
+    this._by.off();
+    this._by = EventInterest.none;
+  }
+
+}
+
+class TrackedValue<T> extends ValueTracker<T> {
+
+  private readonly _on = new EventEmitter<(this: void, newValue: T, oldValue: T) => void>();
+
+  constructor(private _it: T) {
+    super();
+  }
+
+  get on() {
+    return this._on.on;
+  }
+
+  get it(): T {
+    return this._it;
+  }
+
+  set it(value: T) {
+
+    const oldValue = this._it;
+
+    if (oldValue !== value) {
+      this._it = value;
+      this._on.notify(value, oldValue);
+    }
+  }
+
+}
+
+/**
+ * Constructs a value which changes can be tracked.
+ *
+ * @param initial Initial value.
+ *
+ * @returns Value tracker instance.
+ */
+export function trackValue<T>(initial: T): ValueTracker<T>;
+
+/**
+ * Constructs an optional value which changes can be tracked.
+ *
+ * @param initial Initial value.
+ *
+ * @returns Value tracker instance.
+ */
+export function trackValue<T>(initial?: T): ValueTracker<T | undefined>;
+
+export function trackValue<T>(initial: T): ValueTracker<T> {
+  return new TrackedValue<T>(initial);
 }
