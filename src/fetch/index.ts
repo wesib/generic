52db3e511cf5f0496c2a300e61a71e4cc93444f3
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-10-14 20:09:35 +0700

    Get rid of DOM fetch

--- src/fetch/index.ts
+++ src/fetch/index.ts
@@ -1,8 +1,5 @@
 /**
  * @module @wesib/generic
  */
-export * from './dom-fetch';
-export * from './dom-fetch-agent';
-export * from './dom-fetch-result';
 export * from './http-fetch';
 export * from './http-fetch-agent';
