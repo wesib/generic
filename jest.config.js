8bf515351da3c9fa4d2aeddfb950ed4a6f03ab5b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-28 14:55:25 +0700

    Upgrade to `jsdom` v16

--- jest.config.js
+++ jest.config.js
@@ -33,7 +33,7 @@ module.exports = {
     ],
   ],
   restoreMocks: true,
-  testEnvironment: 'jest-environment-jsdom-fifteen',
+  testEnvironment: 'jest-environment-jsdom-sixteen',
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.spec.json',
