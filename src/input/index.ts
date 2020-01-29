c347328f39c529ab29fb051768650a2ab9f018d0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-29 12:08:09 +0700

    Drop `inputValidity()` function

--- src/input/index.ts
+++ src/input/index.ts
@@ -5,6 +5,5 @@
 export * from './convert-input.decorator';
 export * from './default-in-aspects';
 export * from './input-from-control';
-export * from './input-validity';
 export * from './set-input-name.decorator';
 export * from './use-input-element.decorator';
