690635f59e53372d4c67a7c3504ce7d887a3ddfb
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-02-20 15:10:58 +0700

    Page hash URL parameter

---
+++ src/navigation/hash-url/page-hash-url-param.spec.ts
@@ -0,0 +1,65 @@
+import { bootstrapComponents, BootstrapContext, BootstrapWindow, Feature } from '@wesib/wesib';
+import { LocationMock } from '../../spec/location-mock';
+import { Navigation } from '../navigation';
+import { Page } from '../page';
+import { PageHashURLParam } from './page-hash-url-param';
+import { PageHashURLSupport } from './page-hash-url-support.feature';
+
+describe('navigation', () => {
+  describe('PageHashURLParam', () => {
+
+    let locationMock: LocationMock;
+
+    beforeEach(() => {
+      locationMock = new LocationMock();
+      (locationMock.window as any).DOMParser = DOMParser;
+    });
+    afterEach(() => {
+      locationMock.down();
+    });
+
+    let context: BootstrapContext;
+    let navigation: Navigation;
+    let page: Page;
+
+    beforeEach(async () => {
+      @Feature({
+        needs: PageHashURLSupport,
+        setup(setup) {
+          setup.provide({ a: BootstrapWindow, is: locationMock.window });
+        },
+      })
+      class TestFeature {
+      }
+
+      context = await new Promise(bootstrapComponents(TestFeature).whenReady);
+      navigation = context.get(Navigation);
+      navigation.read(p => page = p);
+    });
+
+    it('is equal to page URL initially', () => {
+      expect(page.get(PageHashURLParam).href).toBe(new URL(page.url.origin).href);
+    });
+    it('replaces target page URL hash', async () => {
+
+      const target = await navigation.with(PageHashURLParam, '/hash-path').open('/other#hash');
+
+      expect(target!.url.href).toBe('http://localhost/other#/hash-path');
+      expect(target!.get(PageHashURLParam).href).toBe(new URL('/hash-path', page.url.origin).href);
+    });
+    it('does not alter target page URL page', async () => {
+
+      const target = await navigation.open('/other#hash');
+
+      expect(target!.url.href).toBe('http://localhost/other#hash');
+      expect(target!.get(PageHashURLParam).href).toBe(new URL('/hash', page.url.origin).href);
+    });
+    it('does not alter target page URL page when hash URL set to `null`', async () => {
+
+      const target = await navigation.with(PageHashURLParam, null).open('/other#hash');
+
+      expect(target!.url.href).toBe('http://localhost/other#hash');
+      expect(target!.get(PageHashURLParam).href).toBe(new URL('/hash', page.url.origin).href);
+    });
+  });
+});
---
+++ src/navigation/hash-url/page-hash-url-support.feature.ts
@@ -0,0 +1,47 @@
+/**
+ * @packageDocumentation
+ * @module @wesib/generic
+ */
+import { FeatureDef, FeatureDef__symbol } from '@wesib/wesib';
+import { Navigation } from '../navigation';
+import { NavigationAgent } from '../navigation-agent';
+import { NavigationSupport } from '../navigation-support.feature';
+import { Page } from '../page';
+import { setHashURL } from './hash-url';
+import { PageHashURLValueParam } from './page-hash-url-param.impl';
+
+const PageHashURLSupport__feature: FeatureDef = {
+  needs: NavigationSupport,
+  setup(setup) {
+    setup.provide({ a: NavigationAgent, is: pageHashURLAgent });
+  },
+};
+
+/**
+ * {@link PageHashURLParam Page hash URL parameter} support feature.
+ *
+ * Enables [[NavigationSupport]].
+ */
+export class PageHashURLSupport {
+
+  static get [FeatureDef__symbol](): FeatureDef {
+    return PageHashURLSupport__feature;
+  }
+
+}
+
+function pageHashURLAgent(
+    next: (this: void, target?: Navigation.Target) => void,
+    _when: 'pre-open' | 'pre-replace',
+    _from: Page,
+    to: Page,
+): void {
+
+  const hashURL = to.get(PageHashURLValueParam);
+
+  if (hashURL) {
+    next({ url: setHashURL(to.url, hashURL) });
+  } else {
+    next();
+  }
+}
