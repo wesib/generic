de7ee3900a9f0a1d1b096b94c7ccc2b74bb6aace
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-05-04 17:40:57 +0700

    input: Use `ElementRenderScheduler`

--- src/input/default-in-aspects.ts
+++ src/input/default-in-aspects.ts
@@ -2,11 +2,11 @@
  * @packageDocumentation
  * @module @wesib/generic/input
  */
-import { ContextKey__symbol, ContextValueOpts, ContextValues } from '@proc7ts/context-values';
+import { ContextValueOpts, ContextValues } from '@proc7ts/context-values';
 import { ContextUpKey, ContextUpRef } from '@proc7ts/context-values/updatable';
-import { afterAll, AfterEvent, EventKeeper } from '@proc7ts/fun-events';
+import { AfterEvent, EventKeeper } from '@proc7ts/fun-events';
 import { InConverter, InNamespaceAliaser, InRenderScheduler, intoConvertedBy } from '@proc7ts/input-aspects';
-import { DefaultNamespaceAliaser, DefaultRenderScheduler } from '@wesib/wesib';
+import { DefaultNamespaceAliaser, ElementRenderScheduler } from '@wesib/wesib';
 
 /**
  * Default input aspects.
@@ -14,7 +14,7 @@ import { DefaultNamespaceAliaser, DefaultRenderScheduler } from '@wesib/wesib';
  * This is an `AfterEvent` keeper of aspect converter meant to be applied to controls.
  *
  * As a bare minimum it assigns the following aspects to converted controls:
- * - `InRenderScheduler` set to `DefaultRenderScheduler`,
+ * - `InRenderScheduler` set to `ElementRenderScheduler`,
  * - `InNamespaceAliaser` set to `DefaultNamespaceAliaser.
  *
  * More input aspect converters may be registered in context. They may override the default ones.
@@ -44,15 +44,10 @@ class DefaultInAspectsKey
   ): AfterEvent<[InConverter.Aspect.Factory<any, any>]> {
 
     const nsAlias = opts.context.get(DefaultNamespaceAliaser);
+    const scheduler = opts.context.get(ElementRenderScheduler);
 
-    return afterAll({
-      scheduler: opts.context.get(DefaultRenderScheduler[ContextKey__symbol].upKey),
-      fns: opts.seed,
-    }).keepThru(
-        ({
-            scheduler: [scheduler],
-            fns,
-        }) => intoConvertedBy(
+    return opts.seed.keepThru(
+        (...fns) => intoConvertedBy(
             ...fns,
             InRenderScheduler.to(scheduler),
             InNamespaceAliaser.to(nsAlias),
@@ -63,7 +58,7 @@ class DefaultInAspectsKey
 }
 
 /**
- * A key of bootstrap, definition, or component context containing default input aspects.
+ * A key of component context containing default input aspects.
  */
 export const DefaultInAspects: ContextUpRef<DefaultInAspects, InConverter.Aspect<any, any>> = (
     /*#__PURE__*/ new DefaultInAspectsKey()
