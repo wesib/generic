4de6b3b0775bb6455a438ed4488fc35af3c4871b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-05-06 16:41:49 +0700

    Upgrade to `context-values` v3.1

--- src/hierarchy/hierarchy-registry.impl.ts
+++ src/hierarchy/hierarchy-registry.impl.ts
@@ -1,6 +1,7 @@
 import { nextArgs } from '@proc7ts/call-thru';
-import { ContextRegistry } from '@proc7ts/context-values';
-import { AfterEvent, nextAfterEvent } from '@proc7ts/fun-events';
+import { ContextRegistry, ContextSeedKey } from '@proc7ts/context-values';
+import { ContextUpKey } from '@proc7ts/context-values/updatable';
+import { AfterEvent, EventKeeper, nextAfterEvent } from '@proc7ts/fun-events';
 import { HierarchyContext } from './hierarchy-context';
 
 /**
@@ -10,8 +11,19 @@ export function newHierarchyRegistry<T extends object>(
     up: AfterEvent<[HierarchyContext?]>,
 ): ContextRegistry<HierarchyContext<T>> {
   return new ContextRegistry(
-      key => up.keepThru(
-          upper => upper ? nextAfterEvent(upper.get(key as any)) : nextArgs(),
-      ) as any,
+      <Src, Seed>(key: ContextSeedKey<Src, Seed>) => isContextSeedUpKey(key)
+          ? up.keepThru(
+              upper => upper ? nextAfterEvent(upper.get(key)) : nextArgs(),
+          ) as unknown as Seed
+          : undefined,
   );
+}
+
+/**
+ * @internal
+ */
+function isContextSeedUpKey<Src>(
+    key: ContextSeedKey<Src | EventKeeper<Src[]>, any>,
+): key is ContextUpKey.SeedKey<Src> {
+  return 'upKey' in key;
 }
