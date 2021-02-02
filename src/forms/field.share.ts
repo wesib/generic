c1d0cb9664c8bcca40358e250e93f1fe6234c508
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-02-02 18:28:54 +0700

    `FieldShare` can be used as context key

--- src/forms/field.share.ts
+++ src/forms/field.share.ts
@@ -1,3 +1,4 @@
+import { ContextKey__symbol } from '@proc7ts/context-values';
 import { ComponentShare, ComponentShare__symbol } from '../share';
 import { Field } from './field';
 
@@ -8,5 +9,10 @@ export class FieldShare<TValue = any> extends ComponentShare<Field<TValue>> {
   static [ComponentShare__symbol](): FieldShare {
     return FieldShare$instance || (FieldShare$instance = new FieldShare('field'));
   }
+
+  static get [ContextKey__symbol](): ComponentShare.Key<Field<any>> {
+    return this[ComponentShare__symbol]()[ContextKey__symbol];
+  }
+
 
 }
