3a3b0e80141c233f22859389814e38c7cddbc6ae
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-03-22 17:53:52 +0700

    Add more generic `navElement()` nav link constructor

--- src/navigation/nav-links/nav-anchor.ts
+++ src/navigation/nav-links/nav-anchor.ts
@@ -1,11 +1,6 @@
-import { DomEventDispatcher } from '@frontmeans/dom-events';
-import { css__naming, QualifiedName } from '@frontmeans/namespace-aliaser';
-import { EventReceiver } from '@proc7ts/fun-events';
-import { setOfElements, valueByRecipe } from '@proc7ts/primitives';
-import { Supply } from '@proc7ts/supply';
-import { DefaultNamespaceAliaser, ElementRenderScheduler, RenderDef, Wesib__NS } from '@wesib/wesib';
-import { addCssClass } from '../../util';
-import { Navigation } from '../navigation';
+import { QualifiedName } from '@frontmeans/namespace-aliaser';
+import { RenderDef } from '@wesib/wesib';
+import { navElement } from './nav-element';
 import { NavLink } from './nav-link';
 
 type GenericElement = Element;
@@ -52,17 +47,6 @@ export namespace NavAnchor {
 
 }
 
-const NavAnchor$activeClass: QualifiedName = ['active', Wesib__NS];
-
-const NavAnchor$activeCounters = (/*#__PURE__*/ Symbol('NavAnchor.activeCounter'));
-
-interface NavAnchor$Element extends NavAnchor.Element {
-
-  [NavAnchor$activeCounters]?: { [className: string]: number | undefined };
-
-
-}
-
 /**
  * Creates navigation link for the given anchor element.
  *
@@ -98,76 +82,19 @@ export function navAnchor(
 
 export function navAnchor(
     element:
-        | NavAnchor$Element
-        | ((this: void, owner: NavLink.Owner) => NavAnchor$Element | null | undefined)
+        | NavAnchor.Element
+        | ((this: void, owner: NavLink.Owner) => NavAnchor.Element | null | undefined)
         | null
         | undefined,
     options: NavAnchor.Options = {},
 ): NavLink.Provider {
-
-  const events = setOfElements(options.event || 'click');
-  const { active = NavAnchor$activeClass } = options;
-  let activeClass: string;
-
-  return owner => {
-
-    const anchor: NavAnchor$Element | undefined | null = valueByRecipe(element, owner);
-
-    if (!anchor) {
-      return;
-    }
-
-    const { context, supply: ownerSupply = context.supply } = owner;
-
-    activeClass = css__naming.name(active, context.get(DefaultNamespaceAliaser));
-
-    const navigation = context.get(Navigation);
-    const scheduler = context.get(ElementRenderScheduler);
-    const schedule = scheduler({ node: anchor });
-    const supply = new Supply().needs(ownerSupply);
-    const handleClick: EventReceiver<[Event]> = {
-      supply,
-      receive(_ctx, event) {
-
-        const { href } = anchor;
-        const pageURL = navigation.page.url;
-        const url = new URL(href, anchor.ownerDocument.baseURI);
-
-        if (url.origin !== pageURL.origin) {
-          return; // External link
-        }
-
-        event.preventDefault();
-        if (pageURL.href !== url.href) {
-          navigation.open(href).catch(console.error);
-        }
+  return navElement(
+      element,
+      {
+        ...options,
+        href(element) {
+          return element.href;
+        },
       },
-    };
-    const eventDispatcher = new DomEventDispatcher(anchor);
-
-    supply.cuts(eventDispatcher);
-    for (const event of events) {
-      eventDispatcher.on(event)(handleClick);
-    }
-
-    return ({
-
-      get href(): string {
-        return anchor!.href;
-      },
-
-      supply,
-
-      activate() {
-        return addCssClass(
-            anchor,
-            activeClass,
-            {
-              scheduler: () => schedule,
-            },
-        );
-      },
-
-    });
-  };
+  );
 }
