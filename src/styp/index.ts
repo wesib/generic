b5cbacf5abf2c8afaf6c6e51166f213c477ca075
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-05-14 17:43:26 +0700

    Convert `@ProduceStyle()` to amendment

--- src/styp/index.ts
+++ src/styp/index.ts
@@ -7,6 +7,6 @@ export * from './component-styp-dom.format';
 export * from './component-styp-dom.format-config';
 export * from './component-styp-format';
 export * from './component-styp-renderer';
-export * from './produce-style.decorator';
+export * from './produce-style.amendment';
 export * from './style-producer-support.feature';
 export * from '../theme';
